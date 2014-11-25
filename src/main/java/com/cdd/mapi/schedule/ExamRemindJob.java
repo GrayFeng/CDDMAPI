ff038900597da31a86c4d4d14cc5f35883d6b7a6
GrayF <tyfjy823@gmail.com>
2014-11-25 17:32:38 +0800

    format code

--- src/main/java/com/cdd/mapi/schedule/ExamRemindJob.java
+++ src/main/java/com/cdd/mapi/schedule/ExamRemindJob.java
@@ -25,26 +25,26 @@ import com.google.common.collect.Lists;
 /**
  * Description: ExamRemindJob.java
  * All Rights Reserved.
- * @version 1.0  2014年11月25日 上午11:16:38  
- * @author Gray(jy.feng@zuche.com) 
+ *
+ * @author Gray(jy.feng@zuche.com)
+ * @version 1.0  2014年11月25日 上午11:16:38
  */
 
-public class ExamRemindJob extends QuartzJobBean
-{
-  private Logger log = LoggerFactory.getLogger(ExamRemindJob.class);
+public class ExamRemindJob extends QuartzJobBean {
+    private Logger log = LoggerFactory.getLogger(ExamRemindJob.class);
 
-  protected void executeInternal(JobExecutionContext context) throws JobExecutionException{
-    log.info("---------------------考试提醒分析任务启动-------------------");
-    IRemindService remindService = (IRemindService)SpringConstant.getBean("remindServiceImpl");
-    IBaseService baseService = (IBaseService)SpringConstant.getBean("baseServiceImpl");
-    try {
-      Calendar c = Calendar.getInstance();
-      String startTime = DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
-      c.add(5, 1);
-      String planEndTime = DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
-      c.add(5, 6);
-      String endTime = DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
-      Integer remindCount = remindService.getRemindPageCount4Push(startTime, endTime, ERemindType.EXAM.getCode());
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        log.info("---------------------考试提醒分析任务启动-------------------");
+        IRemindService remindService = (IRemindService) SpringConstant.getBean("remindServiceImpl");
+        IBaseService baseService = (IBaseService) SpringConstant.getBean("baseServiceImpl");
+        try {
+            Calendar c = Calendar.getInstance();
+            String startTime = DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
+            c.add(5, 1);
+            String planEndTime = DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
+            c.add(5, 6);
+            String endTime = DateFormatUtils.format(c.getTime(), "yyyy-MM-dd");
+            Integer remindCount = remindService.getRemindPageCount4Push(startTime, endTime, ERemindType.EXAM.getCode());
 //      if (remindCount.intValue() > 0) {
 //        List<Remind4Push> remindList = null;
 //        for (int i = 1; i <= remindCount.intValue(); i++) {
@@ -56,88 +56,88 @@ public class ExamRemindJob extends QuartzJobBean
 //        }
 //      }
 
-      remindCount = remindService.getRemindPageCount4Push(startTime, endTime, ERemindType.SIGN_UP.getCode());
-      if (remindCount.intValue() > 0) {
-        List<Remind4Push> remindList = null;
-        for (int i = 1; i <= remindCount.intValue(); i++) {
-          remindList = remindService.getRemindList4Push(startTime, endTime, ERemindType.SIGN_UP.getCode(), Integer.valueOf(i));
-          if (remindList != null) {
-        	List<PushMessage> pushMessages = transform(remindList, ERemindType.SIGN_UP);
-            baseService.addPushMsg(pushMessages);
-          }
+            remindCount = remindService.getRemindPageCount4Push(startTime, endTime, ERemindType.SIGN_UP.getCode());
+            if (remindCount.intValue() > 0) {
+                List<Remind4Push> remindList = null;
+                for (int i = 1; i <= remindCount.intValue(); i++) {
+                    remindList = remindService.getRemindList4Push(startTime, endTime, ERemindType.SIGN_UP.getCode(), Integer.valueOf(i));
+                    if (remindList != null) {
+                        List<PushMessage> pushMessages = transform(remindList, ERemindType.SIGN_UP);
+                        baseService.addPushMsg(pushMessages);
+                    }
+                }
+            }
+
+            remindCount = remindService.getRemindPageCount4Push(startTime, planEndTime, ERemindType.LEARING.getCode());
+            if (remindCount.intValue() > 0) {
+                List<Remind4Push> remindList = null;
+                for (int i = 1; i <= remindCount.intValue(); i++) {
+                    remindList = remindService.getRemindList4Push(startTime, planEndTime, ERemindType.LEARING.getCode(), Integer.valueOf(i));
+                    if (remindList != null) {
+                        List<PushMessage> pushMessages = transform(remindList, ERemindType.LEARING);
+                        baseService.addPushMsg(pushMessages);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
-      }
+    }
 
-      remindCount = remindService.getRemindPageCount4Push(startTime, planEndTime, ERemindType.LEARING.getCode());
-      if (remindCount.intValue() > 0) {
-        List<Remind4Push> remindList = null;
-        for (int i = 1; i <= remindCount.intValue(); i++) {
-          remindList = remindService.getRemindList4Push(startTime, planEndTime, ERemindType.LEARING.getCode(), Integer.valueOf(i));
-          if (remindList != null) {
-        	List<PushMessage> pushMessages = transform(remindList, ERemindType.LEARING);
-            baseService.addPushMsg(pushMessages);
-          }
+    private List<PushMessage> transform(List<Remind4Push> remindList, ERemindType type) {
+        List<PushMessage> list = Lists.newArrayList();
+        for (Remind4Push remind4Push : remindList) {
+            PushMessage message = new PushMessage();
+            message.setMemberId(remind4Push.getMemberId());
+            message.setDeviceNo(remind4Push.getDeviceNo());
+            String msg = null;
+            Long diffDay = getDiffDay(remind4Push.getStartTime());
+            switch (type) {
+                case SIGN_UP:
+                    if (diffDay == 1 || diffDay == 3 || diffDay == 7) {
+                        msg = "距离" + remind4Push.getTitle() + "报名开始还有" + diffDay + "天";
+                    } else {
+                        Long endTimeDiffDay = getDiffDay(remind4Push.getEndTime());
+                        if (endTimeDiffDay != null && endTimeDiffDay == 1) {
+                            msg = "距离" + remind4Push.getTitle() + "报名结束还有" + endTimeDiffDay + "天";
+                        }
+                    }
+                    break;
+                case LEARING:
+                    if ((diffDay != null) && (diffDay.longValue() > 0L)) {
+                        msg = "距离" + remind4Push.getTitle() + "还有" + diffDay + "天";
+                    } else {
+                        msg = remind4Push.getTitle() + "开始了！";
+                    }
+                    break;
+                default:
+                    break;
+            }
+            if (msg != null) {
+                message.setMsg(msg);
+                list.add(message);
+            }
         }
-      }
-    } catch (Exception e) {
-      log.error(e.getMessage(), e);
+        return list;
     }
-  }
 
-  private List<PushMessage> transform(List<Remind4Push> remindList,ERemindType type) {
-	  List<PushMessage> list = Lists.newArrayList();
-	  for(Remind4Push remind4Push : remindList){
-		  PushMessage message = new PushMessage();
-	        message.setMemberId(remind4Push.getMemberId());
-	        message.setDeviceNo(remind4Push.getDeviceNo());
-	        String msg = null;
-	        Long diffDay = getDiffDay(remind4Push.getStartTime());
-	        switch (type) {
-	        case SIGN_UP:
-	        	if(diffDay == 1 || diffDay == 3 || diffDay == 7){
-	        		 msg = "距离" + remind4Push.getTitle() + "报名开始还有" + diffDay + "天";
-	        	}else{
-	        		Long endTimeDiffDay = getDiffDay(remind4Push.getEndTime());
-	        		if(endTimeDiffDay != null && endTimeDiffDay == 1){
-	        			msg = "距离" + remind4Push.getTitle() + "报名结束还有" + endTimeDiffDay + "天";
-	        		}
-	        	}
-	          break;
-	        case LEARING:
-	          if ((diffDay != null) && (diffDay.longValue() > 0L)){
-	            msg = "距离" + remind4Push.getTitle() + "还有" + diffDay + "天";
-	          }else {
-	            msg = remind4Push.getTitle() + "开始了！";
-	          }
-	          break;
-			default:
-				break;
-	        }
-	        if(msg != null){
-	        	message.setMsg(msg);
-	        	list.add(message);
-	        }
-	  }
-    return list;
-  }
-
-  private Long getDiffDay(String time) {
-    if (StringUtils.isNotEmpty(time)) {
-      SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
-      try {
-        Date remindTime = sf.parse(time);
-        Calendar c = Calendar.getInstance();
-        c.set(11, 0);
-        c.set(12, 0);
-        c.set(13, 0);
-        c.set(14, 0);
-        long diff = remindTime.getTime() - c.getTimeInMillis();
-        long dayMillis = 86400000L;
-        return Long.valueOf(diff / dayMillis);
-      } catch (ParseException e) {
-        log.error(e.getMessage());
-      }
+    private Long getDiffDay(String time) {
+        if (StringUtils.isNotEmpty(time)) {
+            SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+            try {
+                Date remindTime = sf.parse(time);
+                Calendar c = Calendar.getInstance();
+                c.set(11, 0);
+                c.set(12, 0);
+                c.set(13, 0);
+                c.set(14, 0);
+                long diff = remindTime.getTime() - c.getTimeInMillis();
+                long dayMillis = 86400000L;
+                return Long.valueOf(diff / dayMillis);
+            } catch (ParseException e) {
+                log.error(e.getMessage());
+            }
+        }
+        return null;
     }
-    return null;
-  }
 }
--- src/main/java/com/cdd/mapi/schedule/PushMessageJob.java
+++ src/main/java/com/cdd/mapi/schedule/PushMessageJob.java
@@ -4,7 +4,7 @@ package com.cdd.mapi.schedule;
  * Description: PushMessageJob.java
  * All Rights Reserved.
  * @version 1.0  2014年11月25日 上午11:13:30  
- * @author Gray(jy.feng@zuche.com) 
+ * @author Gray(jy.feng@zuche.com)
  */
 
 import com.cdd.mapi.base.service.IBaseService;
@@ -12,7 +12,9 @@ import com.cdd.mapi.common.SpringConstant;
 import com.cdd.mapi.common.uitls.JPushUtils;
 import com.cdd.mapi.pojo.PushMessage;
 import com.google.common.collect.Lists;
+
 import java.util.List;
+
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.quartz.JobExecutionContext;
@@ -21,52 +23,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
-public class PushMessageJob extends QuartzJobBean{
-	
-  private static boolean working = false;
+public class PushMessageJob extends QuartzJobBean {
 
-  private Logger log = LoggerFactory.getLogger(PushMessageJob.class);
+    private static boolean working = false;
 
-  protected void executeInternal(JobExecutionContext context) throws JobExecutionException{
-    if (working) {
-      log.info("--------------------消息推送任务正在运行中，本次任务注销----------------------");
-      return;
-    }
-    log.info("--------------------消息推送任务启动----------------------");
-    working = true;
-    IBaseService baseService = (IBaseService)SpringConstant.getBean("baseServiceImpl");
-    try {
-      List<PushMessage> msgList = baseService.getPushMsgList();
-      log.info(Thread.currentThread().getName()+"-本次共扫描到待推送消息："+ (CollectionUtils.isEmpty(msgList) ? "0" : Integer.valueOf(msgList.size()))+"条");
-      if ((msgList != null) && (msgList.size() > 0)) {
-        List<Integer> successList = Lists.newArrayList();
-        List<Integer> failList = Lists.newArrayList();
-        for (PushMessage pushMessage : msgList) {
-          if ((StringUtils.isNotEmpty(pushMessage.getDeviceNo())) && (StringUtils.isNotEmpty(pushMessage.getMsg()))){
-            boolean result = JPushUtils.getInstance().sendMsg(pushMessage.getDeviceNo(), pushMessage.getMsg());
-            if (!result) {
-              result = JPushUtils.getInstance().sendMsg(pushMessage.getDeviceNo(), pushMessage.getMsg());
+    private Logger log = LoggerFactory.getLogger(PushMessageJob.class);
+
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        if (working) {
+            log.info("--------------------消息推送任务正在运行中，本次任务注销----------------------");
+            return;
+        }
+        log.info("--------------------消息推送任务启动----------------------");
+        working = true;
+        IBaseService baseService = (IBaseService) SpringConstant.getBean("baseServiceImpl");
+        try {
+            List<PushMessage> msgList = baseService.getPushMsgList();
+            log.info(Thread.currentThread().getName() + "-本次共扫描到待推送消息：" + (CollectionUtils.isEmpty(msgList) ? "0" : Integer.valueOf(msgList.size())) + "条");
+            if ((msgList != null) && (msgList.size() > 0)) {
+                List<Integer> successList = Lists.newArrayList();
+                List<Integer> failList = Lists.newArrayList();
+                for (PushMessage pushMessage : msgList) {
+                    if ((StringUtils.isNotEmpty(pushMessage.getDeviceNo())) && (StringUtils.isNotEmpty(pushMessage.getMsg()))) {
+                        boolean result = JPushUtils.getInstance().sendMsg(pushMessage.getDeviceNo(), pushMessage.getMsg());
+                        if (!result) {
+                            result = JPushUtils.getInstance().sendMsg(pushMessage.getDeviceNo(), pushMessage.getMsg());
+                        }
+                        if (result) {
+                            successList.add(pushMessage.getId());
+                        } else {
+                            failList.add(pushMessage.getId());
+                        }
+                    }
+                }
+                if (successList.size() > 0) {
+                    log.info(new StringBuilder().append(Thread.currentThread().getName()) + "-本次消息推送成功：" + successList.size() + "条");
+                    baseService.updatePushMsgSuccessStatus(successList);
+                }
+                if (failList.size() > 0) {
+                    log.info(new StringBuilder().append(Thread.currentThread().getName()) + "-本次消息推送成功：" + failList.size() + "条");
+                    baseService.updatePushMsgFailStatus(successList);
+                }
             }
-            if (result){
-              successList.add(pushMessage.getId());
-            }else {
-              failList.add(pushMessage.getId());
-            }
-          }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            working = false;
         }
-        if (successList.size() > 0) {
-          log.info(new StringBuilder().append(Thread.currentThread().getName())+"-本次消息推送成功："+successList.size()+"条");
-          baseService.updatePushMsgSuccessStatus(successList);
-        }
-        if (failList.size() > 0) {
-          log.info(new StringBuilder().append(Thread.currentThread().getName())+"-本次消息推送成功："+failList.size()+"条");
-          baseService.updatePushMsgFailStatus(successList);
-        }
-      }
-    } catch (Exception e) {
-      log.error(e.getMessage(), e);
-    } finally {
-      working = false;
     }
-  }
 }
